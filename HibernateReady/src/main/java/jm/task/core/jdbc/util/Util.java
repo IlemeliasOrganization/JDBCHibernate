bdae5e6c4b81990c92458439ad229fec8e3ecd6a
Ilemelias <ilemelias@icloud.com>
2024-06-08 18:06:10 +0300

    hibernate ready

---
+++ HibernateReady/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,45 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String user = "postgres";
+    private static final String password = "0000";
+    private static SessionFactory sessionFactory;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            Properties props = new Properties();
+            props.setProperty("user", user);
+            props.setProperty("password", password);
+            connection = DriverManager.getConnection(url, props);
+        }
+        return connection;
+    }
+    public static SessionFactory getSessionFactory() throws SQLException{
+        if (sessionFactory == null){
+            Configuration config = new Configuration();
+            config.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+            config.setProperty("hibernate.connection.url", url);
+            config.setProperty("hibernate.connection.username", user);
+            config.setProperty("hibernate.connection.password", password);
+            config.addAnnotatedClass(User.class);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(config.getProperties());
+            sessionFactory = config.buildSessionFactory(builder.build());
+        }
+        return sessionFactory;
+    }
+
+}
